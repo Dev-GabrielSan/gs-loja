03193346f9f921def2d457004e4895c97d319563
Dev-GabrielSan <gabriel.ssantos@bandtec.com.br>
2023-08-14 19:05:42 -0300

    backend project created

---
+++ backend/gs_store/src/main/java/com/example/gs_store/GsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.gs_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsStoreApplication.class, args);
+	}
+
+}
